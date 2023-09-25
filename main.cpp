85ed541cccee0aac8b675f431f2a8af45c2bf351
Gilang <jinxprogilang666@gmail.com>
2023-09-25 14:58:05 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "object.hpp"
+#include <iostream>
+
+int main() {
+
+  Song back_to_black("Back to Black", "Amy Winehouse");
+
+  std::cout << back_to_black.get_title() << "\n";
+  std::cout << back_to_black.get_artist() << "\n";
+}
